de0dd9c0294d1e7fbd5b910ca9a74d62bee8cd45
vikshiyan <miss.shiyan@gmail.com>
2024-10-23 00:07:27 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,131 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useEffect, useState } from 'react';
 
-const USER_ID = 0;
+import * as todoFromService from './api/todos';
+import { Todo } from './types/Todo';
+import { getFilteredTodos } from './utils/getFilterdTodos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Error } from './components/Error';
+import { TypeError } from './types/TypeError';
+import { SelectedType } from './types/SelectedType';
+import { Header } from './components/Header';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<TypeError>(
+    TypeError.DEFAULT,
+  );
+  const [selectedOption, setSelectedOption] = useState<SelectedType>(
+    SelectedType.ALL,
+  );
+  const [newTitle, setNewTitle] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [temptTodo, setTempTodo] = useState<Todo | null>(null);
+  const [loadingTodoId, setLoadingTodoId] = useState<number[]>([]);
+  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
+
+  const filteredTodos = getFilteredTodos(todos, selectedOption);
+
+  const handleAddTodo = (newTodo: Omit<Todo, 'id'>) => {
+    todoFromService
+      .createTodo(newTodo)
+      .then(addedTodo => {
+        setTodos(currentTodos => [...currentTodos, addedTodo]);
+        setNewTitle('');
+      })
+      .catch(() => {
+        setErrorMessage(TypeError.ADDING);
+      })
+      .finally(() => {
+        setLoading(false);
+        setTempTodo(null);
+      });
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    setLoading(true);
+    setLoadingTodoId(currentIds => [...currentIds, todoId]);
+    todoFromService
+      .deleteTodo(todoId)
+      .then(() => {
+        setTodos(currentTodos =>
+          currentTodos.filter(todo => todo.id !== todoId),
+        );
+      })
+      .catch(() => {
+        setErrorMessage(TypeError.DELETING);
+      })
+      .finally(() => {
+        setLoading(false);
+        setLoadingTodoId([]);
+      });
+  };
+
+  const updateTodo = (updatedTodo: Todo) => {
+    setLoadingTodoId(currentIds => [...currentIds, updatedTodo.id]);
+    todoFromService
+      .updateTodo(updatedTodo)
+      .then(newTodo => {
+        setTodos(currentTodos =>
+          currentTodos.map(todo => (newTodo.id === todo.id ? newTodo : todo)),
+        );
+        setEditingTodoId(null);
+      })
+      .catch(() => {
+        setErrorMessage(TypeError.UPDATING);
+      })
+      .finally(() => {
+        setLoadingTodoId([]);
+      });
+  };
+
+  useEffect(() => {
+    todoFromService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => setErrorMessage(TypeError.LOADING));
+  }, []);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          newTitle={newTitle}
+          setNewTitle={setNewTitle}
+          setErrorMessage={setErrorMessage}
+          onAddTodo={handleAddTodo}
+          setLoading={setLoading}
+          loading={loading}
+          setTempTodo={setTempTodo}
+          updateTodo={updateTodo}
+        />
+
+        {todos.length > 0 && (
+          <>
+            <TodoList
+              todos={filteredTodos}
+              temptTodo={temptTodo}
+              onDeleteTodo={handleDeleteTodo}
+              loadingTodoId={loadingTodoId}
+              updateTodo={updateTodo}
+              editingTodoId={editingTodoId}
+              setEditingTodoId={setEditingTodoId}
+            />
+
+            <Footer
+              todos={todos}
+              selectedOption={selectedOption}
+              setSelectedOption={setSelectedOption}
+              handleDeleteTodo={handleDeleteTodo}
+            />
+          </>
+        )}
+      </div>
+
+      <Error errorMessage={errorMessage} setErrorMessage={setErrorMessage} />
+    </div>
   );
 };
